aa457f01d4d3ca0fbb30c3bb7df64dbdd96c7432
FlechitUp <ximena.pocco@ucsp.edu.pe>
2019-05-27 23:41:14 -0400

    Register

---
+++ hbpro/README.txt
@@ -0,0 +1,5 @@
+Activate virtual environment:
+    source activate DjangoProject
+
+Run server:
+    python manage.py runserver
---
+++ hbpro/apps/register/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from apps.register.views import index
+
+urlpatterns = [
+    url(r'^$',index),
+    
+]
