d510e2bf861fde3d2f78a86016ac07f7c8a58ec8
FlechitUp <ximena.pocco@ucsp.edu.pe>
2019-06-20 23:43:46 -0400

    logim redirect

--- hbpro/apps/login/views.py
+++ hbpro/apps/login/views.py
@@ -1,40 +1,37 @@
-from django.shortcuts import render
+from django.db.models import Q
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 #from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from .form import loginForm
+from apps.user.models import Student2, Mentor
+from django.contrib import messages
 
 def login(request):
 	#import pudb;	pudb.set_trace()
 	#context = RequestContext(request)
 	if request.method == 'POST':
 		form = loginForm(request.POST)
-		user = authenticate(username=form.fields['nick_name'], password=form.fields['password'])
-		print("oksoks!!!!!!!!!!!!!!!!!!!!!!!", form.fields['nick_name'])
-		if user is not None:
-			if user.is_active:
-				login(request, user)
-				# Redirect to index page.
-				#return HttpResponseRedirect("rango/")
-				return render(request, 'initpage/index.html')
-			else:
-				# Return a 'disabled account' error message
-				#return HttpResponse("You're account is disabled.")
-				return render(request, 'login/index.html')
+		#user = authenticate(username=form.fields['nick_name'], password=form.fields['password'])
+		username = request.POST.get('nick_name')
+		password = request.POST.get('password')
+		print("oksoks!!!!!!!!!!!!!!!!!!!!!!!", username)	
+		try:
+			stu = Student2.objects.get(Q(nick_name=username) & Q(password=password))
+			print(stu)
+			return render(request, 'contentuser/index.html')
+
+		except Student2.DoesNotExist:
+			print("Either the entry or blog doesn't exist.")
+			print('!!!!!!!--***********no estas en la bd')		
+			messages.warning(request, ' nickname or password does not exist')
+			return redirect('login')
+
+		"""if(username =="u1"):
+			return render(request, 'contentuser/index.html')
 		else:
 			print('!!!!!!!--***********no estas en la bd')
-			return render(request, 'login/index.html')
-		"""if form.is_valid():
-			form.save()
-			name = form.cleaned_data['nick_name']
-			print(name)
-			return render(request, 'initpage/index.html')"""
-		"""
-								cont.name=request.POST['name']
-								cont.last_name=request.POST['email']
-								cont.github=request.POST['name']
-								cont.is_working=request.POST['name']
-								cont.save()"""
+			return render(request, 'login/index.html')	"""
 	else:
 		form = loginForm()
 		return render(request, 'login/index.html', {'form': form})
